4edc921f7c03a1cfa6af993345e4a67c0d655656
Sampanna Mohite <sampanu@gmail.com>
2017-03-08 12:28:48 +0000

    Changes to test.java

--- FAAST_Test/src/main/java/com/FAAST/Test/summay_pageObjects/test.java
+++ FAAST_Test/src/main/java/com/FAAST/Test/summay_pageObjects/test.java
@@ -1,8 +1,14 @@
 package com.FAAST.Test.summay_pageObjects;
 
+import static java.lang.System.*;
+
 /**
  * Created by sampanna on 08/03/2017.
  */
 public class test {
     //some dummy code
+    public static void funct(){
+        System.out.println("test git!!");
+    }
+
 }
